2e9c3cf933b870229722c6ed1e7766e6ba17e2c1
Jaakko Nurkka <jaakkonurkka@gmail.com>
2020-11-13 10:38:55 +0100

    Log lifecycle of activity

--- app/src/main/java/com/cliniserve/stepcounter/MainActivity.kt
+++ app/src/main/java/com/cliniserve/stepcounter/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.cliniserve.stepcounter
 
-import android.os.Bundle
+import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.content.Context;
 
-class MainActivity() {
+class MainActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Log.d("StepCounter", "Initiated app")
+  }
 
-val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+  override fun onStart() {
+    super.onStart()
+    Log.d("StepCounter", "Started app")
+  }
 
+  override fun onResume() {
+    super.onResume()
+    Log.d("StepCounter", "Resumed app")
+  }
 
+  override fun onPause() {
+    super.onPause()
+    Log.d("StepCounter", "Paused app")
+  }
+
+  override fun onStop() {
+    super.onStop()
+    Log.d("StepCounter", "Stopped app")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.d("StepCounter", "Destroyed app")
+  }
 }
